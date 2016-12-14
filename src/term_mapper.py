56c3ac279272c6f35a028f3957be99dc6009f000
jmcquown <jmcq521@gmail.com>
2016-12-14 17:19:28 -0500

    Sorted output based on frequency

--- src/term_mapper.py
+++ src/term_mapper.py
@@ -5,13 +5,13 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-
+    
     #input is - document name::word\tfrequency
     doc_word, count = line.split('\t', 1)
     #Get the document and word from previous .split
     doc_name, word = doc_word.split(':')
     #Join the document and frequency
-    value = ":".join([doc_name, count])
+    # value = ":".join([count, doc_name])
 
     #Output as word\tdocument:frequency
-    print ('%s\t%s') % (word, value)
+    print ('%s\t%s:%s') % (word, doc_name, count)
--- src/term_reducer.py
+++ src/term_reducer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from operator import itemgetter
 
 #input - word\tdocument:frequency
 #ouput - word: document1\tfrequency, document2\tfrequency, etc.
@@ -8,7 +9,7 @@ import sys
 current_word = None
 value = None
 word = None
-
+doc_freq_table = []
 
 for line in sys.stdin:
     #Strip \n off the right side
@@ -17,14 +18,36 @@ for line in sys.stdin:
 
     #Get the word and document:frequency
     word, doc_freq = line.split('\t')
+    doc_name, frequency = doc_freq.split(':')
+
+    #Cast is necessary here in order for the .sort() to work later
+    frequency = int(frequency)
 
     #Same idea as the word count reducer
     if current_word == word:
-        value = ', '.join([doc_freq, value])
+        doc_freq_table.append([doc_name, frequency])
     else:
         if current_word:
-            print('%s\t%s') % (current_word, value)
+            #Sort the list by frequency in descending order
+            doc_freq_table.sort(key=itemgetter(1), reverse=True)
+            
+            print word,
+            #Iterate through the sorted list
+            for value in doc_freq_table:
+                print('%s:%s') % (value[0], value[1]),
+            print('\n')
+            #Clear the list
+            doc_freq_table = []
+
         current_word = word
-        value = doc_freq
+        doc_freq_table.append([doc_name, frequency])
 if current_word == word:
-    print('%s\t%s') % (current_word, value)
+    #Sort the list by frequency in descending order
+    doc_freq_table.sort(key=itemgetter(1), reverse=True)
+    
+    print doc_name,
+    #Iterate through the sorted list
+    for value in doc_freq_table:
+        print('%s:%s') % (value[0], value[1]),
+    print('\n')
+    doc_freq_table = []
