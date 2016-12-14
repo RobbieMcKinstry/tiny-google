cbd1df17371e26a9af409d40d121fa2190eb7ac3
jmcquown <jmcq521@gmail.com>
2016-12-14 00:41:14 -0500

    Fiddling with shit

--- src/term_mapper.py
+++ src/term_mapper.py
@@ -2,10 +2,6 @@
 
 import sys
 
-# doc_name = sys.stdin.readline().strip()
-# print ('doc_name - %s') % doc_name
-doc_name = "/mounted/docs/doc.txt"
-
 
 for line in sys.stdin:
     line = line.strip()
@@ -13,6 +9,6 @@ for line in sys.stdin:
     #input is - document name::word\tfrequency
     doc_word, count = line.split('\t', 1)
     # print('doc_word --> %s') % (doc_word)
-    doc_name, word = doc_word.split('::')
+    doc_name, word = doc_word.split(':')
 
     print ('%s\t%s:%s') % (word, doc_name, count)
--- src/word_count_reducer.py
+++ src/word_count_reducer.py
@@ -7,41 +7,44 @@ current_word = None
 overall_count = 0
 word = None
 
-#Get filename
-doc_name = sys.stdin.readline().strip()
-print ('doc_name: %s') % (doc_name)
-# doc_name = "mounted/docs/doc.txt"
+try:
+    #get input from Stdin
+    for line in sys.stdin:
+        line = line.strip()
 
-#get input from Stdin
-for line in sys.stdin:
-    line = line.strip()
+        # print line
 
-    # print line
+        #get the tuple with the word and 1
+        word, count = line.split('\t', 1)
 
-    #get the tuple with the word and 1
-    word, count = line.split('\t', 1)
+        #convert count from string to int
+        try:
+            count = int(count)
+        except ValueError:
+            continue
 
-    #convert count from string to int
-    try:
-        count = int(count)
-    except ValueError:
-        continue
+        #Note: Hadoop will sort the output of mapper based on the key (word)
+        #   therefore once we don't see a word anymore from Stdin, it will not appear again
+        #check if the current_word = word
+        if current_word == word:
+            overall_count += count
+        else:
+            if current_word:
+                #ouput Filename:Word, Frequency
+                print('%s\t%s') % (current_word, overall_count)
+            current_word = word
+            overall_count = count
 
-    #Note: Hadoop will sort the output of mapper based on the key (word)
-    #   therefore once we don't see a word anymore from Stdin, it will not appear again
-    #check if the current_word = word
+    #print out last word
     if current_word == word:
-        overall_count += count
-    else:
-        if current_word:
-            #ouput Filename:Word, Frequency
-            print('%s:%s\t%s') % (doc_name, current_word, overall_count)
-            # print('%s\t%s') % (current_word, overall_count)
-        current_word = word
-        overall_count = count
-
-#print out last word
-if current_word == word:
-    print('%s:%s\t%s') % (doc_name, current_word, overall_count)
-    # print('%s\t%s') % (current_word, overall_count)
+        print('%s\t%s') % (current_word, overall_count)
+except IOError:
+    try:
+        sys.stdout.close()
+    except:
+        pass
+    try:
+        sys.sterr.close()
+    except:
+        pass
 
