11b7470d489423950da3f27b91b22836dfcebd16
jmcquown <jmcq521@gmail.com>
2016-12-16 11:33:26 -0500

    term_reducer now outputs to .json

--- src/term_reducer.py
+++ src/term_reducer.py
@@ -2,6 +2,7 @@
 
 import sys
 from operator import itemgetter
+import json
 
 #input - word\tpath**document:frequency
 #ouput - word: 
@@ -17,6 +18,7 @@ current_word = None
 value = None
 word = None
 doc_freq_table = []
+word_dict = {}
 
 for line in sys.stdin:
     #Strip \n off the right side
@@ -43,12 +45,16 @@ for line in sys.stdin:
         if current_word:
             #Sort the list by frequency in descending order
             doc_freq_table.sort(key=itemgetter(2), reverse=True)
+
+            word_dict[current_word] = doc_freq_table
             
-            print('%s\n{\n') % (current_word),
-            #Iterate through the sorted list
-            for value in doc_freq_table:
-                print('Document:\t%s\nPath:\t%s\nFrequency:\t%s\n') % (value[0], value[1], value[2]),
-            print('}\n')
+            # print('%s\n{\n') % (current_word),
+            # #Iterate through the sorted list
+            # for value in doc_freq_table:
+            #     print('Document:\t%s\nPath:\t%s\nFrequency:\t%s\n') % (value[0], value[1], value[2]),
+            # print('}\n')
+
+
             #Clear the list
             doc_freq_table = []
 
@@ -57,10 +63,17 @@ for line in sys.stdin:
 if current_word == word:
     #Sort the list by frequency in descending order
     doc_freq_table.sort(key=itemgetter(2), reverse=True)
+
+    word_dict[current_word] = doc_freq_table
     
-    print('%s\n{\n') % (current_word),
-    #Iterate through the sorted list
-    for value in doc_freq_table:
-        print('Document:\t%s\nPath:\t%s\nFrequency:\t%s\n') % (value[0], value[1], value[2]),
-    print('}\n')
+    # print('%s\n{\n') % (current_word),
+    # #Iterate through the sorted list
+    # for value in doc_freq_table:
+    #     print('Document:\t%s\nPath:\t%s\nFrequency:\t%s\n') % (value[0], value[1], value[2]),
+    # print('}\n')
+
     doc_freq_table = []
+
+#Dump the dictionary as a json
+with open('result.json', 'w') as fp:
+    json.dump(word_dict, fp)
