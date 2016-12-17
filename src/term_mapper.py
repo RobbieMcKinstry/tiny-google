ed4eb365d997412e39701c8d5240084a1ae8af39
jmcquown <jmcq521@gmail.com>
2016-12-16 20:15:05 -0500

    Removed full file path, replaced with /

--- src/term_mapper.py
+++ src/term_mapper.py
@@ -4,12 +4,14 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    #input is - document name::word\tfrequency
+    #input is - document name:word\tfrequency
     doc_word, count = line.split('\t', 1)
     #Get the document and word from previous .split
     #I got an error if I didn't do the split this way...
     splurg = doc_word.split(':')
     doc_name = splurg[-2]
+    doc_name = doc_name.split('/')[-1]
+    doc_name = '/' + doc_name
     word = splurg[-1]
 
     #Output as word\tdocument:frequency
